2343ff6652403cd5979a1811dade09d5a355d83d
Ralph Cacho <cacho.rc@gmail.com>
2018-03-31 22:10:53 -0700

    Add search method

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    var accessToken = '';
+    //var myHeaders = new Headers();
+
+    var myOptions = {
+      method: 'GET',
+      headers:  {
+        'Authorization': 'Bearer ' + accessToken
+     },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions )
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,8 +40,17 @@ class App extends Component {
             <FormControl
               type="text"
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => {
+                this.setState({query: event.target.value})
+              }}
+              onKeyPress={ event => {
+                if(event.key === 'Enter') {
+                  this.search()
+                }
+              }}
             />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
